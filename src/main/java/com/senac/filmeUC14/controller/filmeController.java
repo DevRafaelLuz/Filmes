a573ae869a9c2c5d7db18c58022d6df15650d097
RafaelLuz <rafael.luz2005@gmail.com>
2023-11-10 19:22:02 -0300

    Criando página de cadastro e de listagem

--- src/main/java/com/senac/filmeUC14/controller/filmeController.java
+++ src/main/java/com/senac/filmeUC14/controller/filmeController.java
@@ -1,12 +1,39 @@
 package com.senac.filmeUC14.controller;
 
+import com.senac.filmeUC14.model.Filme;
+import java.util.ArrayList;
+import java.util.List;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 
 @Controller
-public class filmeController {
+public class FilmeController {
+    private List<Filme> listaFilme = new ArrayList();
+    
     @GetMapping("/pagina-inicial")
     public String ExibirPagInicial() {
         return "index";
+    }
+    
+    @GetMapping("/pagina-cadastro")
+    public String cadastroForm(Model model) {
+        model.addAttribute("filme", new Filme());
+        return "cadastro";
+    }
+    
+    @GetMapping("/pagina-listagem")
+    public String listagem(Model model) {
+        model.addAttribute("lista", listaFilme);
+        return "lista";
+    }
+    
+    @PostMapping("/registrar")
+    public String recebeCadastro(@ModelAttribute Filme filme, Model model) {
+        filme.setId(listaFilme.size() + 1);
+        listaFilme.add(filme);
+        return "redirect:/pagina-listagem";
     }
 }
