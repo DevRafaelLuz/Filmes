68a4f1e4228d024e11db57aed1adb1d34e67b44a
RafaelLuz <rafael.luz2005@gmail.com>
2023-11-10 15:53:28 -0300

    Criando página inicial

--- src/main/java/com/senac/filmeUC14/controller/filmeController.java
+++ src/main/java/com/senac/filmeUC14/controller/filmeController.java
@@ -1,5 +1,12 @@
 package com.senac.filmeUC14.controller;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
 public class filmeController {
-    
+    @GetMapping("/pagina-inicial")
+    public String ExibirPagInicial() {
+        return "index";
+    }
 }
