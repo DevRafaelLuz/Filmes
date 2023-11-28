fea9eaefc1467bdc26d5cc36fd593b74cc746f81
RafaelLuz <rafael.luz2005@gmail.com>
2023-11-27 22:19:08 -0300

    Cookies adicionados para mudar o tema das páginas

--- src/main/java/com/senac/filmeUC14/controller/filmeController.java
+++ src/main/java/com/senac/filmeUC14/controller/filmeController.java
@@ -2,17 +2,22 @@ package com.senac.filmeUC14.controller;
 
 import com.senac.filmeUC14.model.Analise;
 import com.senac.filmeUC14.model.Filme;
+import com.senac.filmeUC14.model.Tema;
 import com.senac.filmeUC14.service.AnaliseService;
 import com.senac.filmeUC14.service.FilmeService;
+import jakarta.servlet.http.Cookie;
+import jakarta.servlet.http.HttpServletResponse;
 import java.util.ArrayList;
 import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.CookieValue;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.servlet.ModelAndView;
 
 @Controller
 public class FilmeController {
@@ -22,23 +27,60 @@ public class FilmeController {
     @Autowired
     AnaliseService analiseService;
     
+    //Página inicial
+    @PostMapping("/tema-inicial")
+    public ModelAndView gravaTema(@ModelAttribute Tema tema, HttpServletResponse response) {
+        Cookie cookieTema = new Cookie("tema-pagina", tema.getTema());
+        cookieTema.setDomain("localhost"); 
+        cookieTema.setHttpOnly(true); 
+        cookieTema.setMaxAge(86400);
+        response.addCookie(cookieTema);
+        return new ModelAndView("redirect:/pagina-inicial");
+    }
+    
     @GetMapping("/pagina-inicial")
-    public String ExibirPagInicial() {
+    public String ExibirPagInicial(@CookieValue(name="tema-pagina", defaultValue="claro")String tema, Model model) {
+        model.addAttribute("css", tema);
         return "index";
     }
     
+    //Página cadastro
+    @PostMapping("/tema-cadastro")
+    public ModelAndView gravaTemaCad(@ModelAttribute Tema tema, HttpServletResponse response) {
+        Cookie cookieTema = new Cookie("tema-pagina", tema.getTema());
+        cookieTema.setDomain("localhost"); 
+        cookieTema.setHttpOnly(true); 
+        cookieTema.setMaxAge(86400);
+        response.addCookie(cookieTema);
+        return new ModelAndView("redirect:/pagina-cadastro");
+    }
+    
     @GetMapping("/pagina-cadastro")
-    public String cadastroForm(Model model) {
+    public String cadastroForm(@CookieValue(name="tema-pagina", defaultValue="claro")String tema, Model model) {
+        model.addAttribute("css", tema);
         model.addAttribute("filme", new Filme());
         return "cadastro";
     }
     
+    //Página listagem
+    @PostMapping("/tema-listagem")
+    public ModelAndView gravaTemaList(@ModelAttribute Tema tema, HttpServletResponse response) {
+        Cookie cookieTema = new Cookie("tema-pagina", tema.getTema());
+        cookieTema.setDomain("localhost"); 
+        cookieTema.setHttpOnly(true); 
+        cookieTema.setMaxAge(86400);
+        response.addCookie(cookieTema);
+        return new ModelAndView("redirect:/pagina-listagem");
+    }
+    
     @GetMapping("/pagina-listagem")
-    public String listagem(Model model) {
+    public String listagem(@CookieValue(name="tema-pagina", defaultValue="claro")String tema, Model model) {
+        model.addAttribute("css", tema);
         model.addAttribute("lista", filmeService.listarTodos());
         return "lista";
     }
     
+    //Registrar
     @PostMapping("/registrar")
     public String recebeCadastro(@ModelAttribute Filme filme, Model model) {  
         if (filme.getId() != null) {
@@ -56,8 +98,20 @@ public class FilmeController {
         return "redirect:/pagina-listagem";
     }
     
+    
+    //Página detalhes
+    @PostMapping("/tema-detalhes")
+    public ModelAndView gravaTemaDetalhes(@ModelAttribute Tema tema, HttpServletResponse response) {
+        Cookie cookieTema = new Cookie("tema-pagina", tema.getTema());
+        cookieTema.setDomain("localhost"); 
+        cookieTema.setHttpOnly(true); 
+        cookieTema.setMaxAge(86400);
+        response.addCookie(cookieTema);
+        return new ModelAndView("redirect:/detalhes");
+    }
+    
     @GetMapping("/detalhes")
-    public String exibiDetalhesFilme(Model model, @RequestParam String id) {
+    public String exibiDetalhesFilme(@CookieValue(name="tema-pagina", defaultValue="claro")String tema, Model model, @RequestParam String id) {
         Integer idFilme = Integer.parseInt(id);
         
         Filme filmeEncontrado = new Filme();
@@ -66,21 +120,34 @@ public class FilmeController {
         List<Analise> analisesEncontradas = new ArrayList<>();
         analisesEncontradas = analiseService.listar(idFilme);
         
-        
+        model.addAttribute("css", tema);
         model.addAttribute("filme", filmeEncontrado);
         model.addAttribute("analise", new Analise());
         model.addAttribute("analises", analisesEncontradas);
         return "detalhes";
     }
     
+    //Página alterar
+    @PostMapping("/tema-alterar")
+    public ModelAndView gravaTemaAlterar(@ModelAttribute Tema tema, HttpServletResponse response) {
+        Cookie cookieTema = new Cookie("tema-pagina", tema.getTema());
+        cookieTema.setDomain("localhost"); 
+        cookieTema.setHttpOnly(true); 
+        cookieTema.setMaxAge(86400);
+        response.addCookie(cookieTema);
+        return new ModelAndView("redirect:/alterar");
+    }
+    
     @GetMapping("/alterar")
-    public String alterarFilme(@RequestParam String id, Model model) {
+    public String alterarFilme(@RequestParam String id, @CookieValue(name="tema-pagina", defaultValue="claro")String tema, Model model) {
         Integer idFilme = Integer.parseInt(id);
         Filme filmeEncontrado = filmeService.buscarPorId(idFilme);
+        model.addAttribute("css", tema);
         model.addAttribute("filme", filmeEncontrado);
         return "alterar";
     }
     
+    //Excluir
     @GetMapping("/excluir")
     public String excluirFilme(@RequestParam String id) {
         Integer idFilme = Integer.parseInt(id);
@@ -94,5 +161,5 @@ public class FilmeController {
         Integer idAnalise = Integer.parseInt(id);
         analiseService.excluir(idAnalise);
         return "redirect:/pagina-listagem";
-    }   
+    }    
 }
---
+++ src/main/java/com/senac/filmeUC14/model/Tema.java
@@ -0,0 +1,8 @@
+package com.senac.filmeUC14.model;
+
+import lombok.Data;
+
+@Data
+public class Tema {
+    private String tema;
+}
